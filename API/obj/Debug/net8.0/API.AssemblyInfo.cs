8fe5d6dad0e31241bd799fb90bbd8a6b506ab402
Daniel Bogatu <daniel.bogatu@amplified.software>
2024-09-24 19:53:18 +0200

    Set

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d498dab7d5bbab4d3ae45bccfa8c362ccfaa0f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633f7f16951db2421b3a4b3f1214359a6a440f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae86493b5b744279e70fc238237261364a3ee628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633f7f16951db2421b3a4b3f1214359a6a440f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
