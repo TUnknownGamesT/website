633f7f16951db2421b3a4b3f1214359a6a440f50
Daniel Bogatu <daniel.bogatu@amplified.software>
2024-09-24 19:52:12 +0200

    Set port

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://localhost:5000");
 // Add services to the container.
 
 builder.Services.AddControllers();
--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae86493b5b744279e70fc238237261364a3ee628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d498dab7d5bbab4d3ae45bccfa8c362ccfaa0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
