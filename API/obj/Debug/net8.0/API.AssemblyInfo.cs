110ef27b866f0bf346b473ecc14af102106a31fd
Daniel Bogatu <daniel.bogatu@amplified.software>
2024-09-25 11:48:50 +0200

    Fix5

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4abb6be6b5563341b3f32f5d77dbd3cb9b2dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee18d5c05533416e2b19e550dcd983aa4caa6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
